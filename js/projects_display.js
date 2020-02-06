887913af05eb4458d79c0b1672a1bb47728bc53b
Tris <biggybi@gmail.com>
2020-02-06 23:04:35 +0100

    disable link over project

--- js/projects_display.js
+++ js/projects_display.js
@@ -1,6 +1,6 @@
 function projectTemplate(projectTemplate) {
 	return `<div class="project" id="${projectTemplate.id}">
-		<a class="projecturl" href="#"></a>
+		<a class="projecturl" href="javascript:void(0)"></a>
 		<div class="projectHeader">
 			<h3 class="lang_en lang_active">${projectTemplate.name_en}</h3>
 			<h3 class="lang_fr lang_inactive">${projectTemplate.name_fr}</h3>
